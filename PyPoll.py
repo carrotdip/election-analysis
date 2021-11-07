0986af83e6962e2f506b1ad77d311d48b8fa5316
carolinediep <92421274+carrotdip@users.noreply.github.com>
2021-11-06 18:36:49 -0700

    Update README.md

--- PyPoll.py
+++ PyPoll.py
@@ -14,6 +14,9 @@ file_to_load = os.path.join("Resources","election_results.csv")
 #Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis","election_analysis.txt")
 
+#Initialize a total vote counter.
+total_votes = 0
+
 #Open the election results and read the file
 with open(file_to_load) as election_data:
     #To do: perform analysis
@@ -22,7 +25,10 @@ with open(file_to_load) as election_data:
     #Print the header row.
     headers = next(file_reader)
     print(headers)
-    
+    #Print each row in the CSV file.
+    for row in file_reader:
+        total_votes += 1
+        print(total_votes)
     print(election_data)
 
 
