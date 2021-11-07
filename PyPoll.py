0838e627e30380102ca8526da7be7c223cb68ec9
carolinediep <92421274+carrotdip@users.noreply.github.com>
2021-11-07 15:29:15 -0800

    2nd commit

--- PyPoll.py
+++ PyPoll.py
@@ -16,6 +16,15 @@ file_to_save = os.path.join("analysis","election_analysis.txt")
 
 #Initialize a total vote counter.
 total_votes = 0
+#Declare a new list for Candidate Options.
+candidate_options = []
+#Declare the empty dictionary to store election results
+candidate_votes = {}
+
+#Winnning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 
 #Open the election results and read the file
 with open(file_to_load) as election_data:
@@ -24,26 +33,56 @@ with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
     #Print the header row.
     headers = next(file_reader)
-    print(headers)
+    #--print(headers)
     #Print each row in the CSV file.
     for row in file_reader:
         total_votes += 1
-        print(total_votes)
-    print(election_data)
-
-
-#Using the open() function with the "w" mode to open a file
-#outfile = open (file_to_save,"w")
-    #Write some data to the file
-    #outfile.write("Hello World")
-#Close the file
-#outfile.close()
-
-#Using the with statement, open the file as a text file
-with open (file_to_save, "w") as txt_file:
-#   Write some data to the file
-#   txt_file.write("Arapahoe, ")
-#   txt_file.write("Denver, ")
-#   txt_file.write("Jefferson")
-#Write three counties to the file
-    txt_file.write("Counties in the Election\n-------------------------\nArapahoe\nDenver\nJefferson")
+        #--print(total_votes)
+    #Print the candidate name from each row
+        candidate_name = row[2]
+    #Add the candidate name to the candidate list...
+    #If the candidate does not match any existing candidate.
+        if candidate_name not in candidate_options:
+            #Add it to the list of candidates
+            candidate_options.append(candidate_name)
+            #Begin tracking the candidate's vote count.
+            candidate_votes[candidate_name] = 0
+        #Add a vote to that candidate's count.
+        candidate_votes[candidate_name] += 1
+        #Save the results to out text file.
+        with open(file_to_save, "w") as txt_file:
+        # Print the final vote count to the terminal.
+         election_results = (
+            f"\nElection Results\n"
+            f"-------------------------\n"
+            f"Total Votes: {total_votes:,}\n"
+            f"-------------------------\n")
+    print(election_results, end="")
+    # Save the final vote count to the text file.
+    txt_file.write(election_results)
+    # Determine the percentage of votes for each candidate by looping through the counts.
+    # Iterate through the candidate list.
+    for candidate_name in candidate_votes:
+        # 2. Retrieve vote count of a candidate.
+        votes = candidate_votes[candidate_name]
+        # 3. Calculate the percentage of votes.
+        vote_percentage = float(votes) / float(total_votes) * 100
+        # 4. Print the candidate name and percentage of votes.
+        print(f"{candidate_name} received {vote_percentage:.1f}% of the vote")
+        #Determine the winning vote count and candidate.
+        #Determine if the votes is greater than the winning count....WHY?
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            #If true, set winning_count = votes amd winning_percentage = vote_percentage
+            winning_count = votes
+            winning_percentage = vote_percentage
+            #And, set the winning_candidate = candidate's name
+            winning_candidate = candidate_name
+        #To do: print out the winning candidate, vote count, and percentage
+    #    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+    #print(winning_candidate_summary)
